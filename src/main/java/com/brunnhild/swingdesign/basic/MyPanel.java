4c5938d5cfb381a6b841ef9aef493eb1f2473380
Brunnhild <10175101259@stu.ecnu.edu.cn>
2019-10-23 22:54:30 +0800

    :tada: the initial commit

---
+++ src/main/java/com/brunnhild/swingdesign/basic/MyPanel.java
@@ -0,0 +1,11 @@
+package com.brunnhild.swingdesign.basic;
+
+import javax.swing.*;
+
+class MyPanel extends JPanel {
+
+    MyPanel() {
+        super();
+        add(new JLabel("Swing Design"));
+    }
+}
---
+++ src/main/java/com/brunnhild/swingdesign/darcula/MyPanel.java
@@ -0,0 +1,11 @@
+package com.brunnhild.swingdesign.darcula;
+
+import javax.swing.*;
+
+class MyPanel extends JPanel {
+
+    MyPanel() {
+        super();
+        add(new JLabel("Swing Design"));
+    }
+}
